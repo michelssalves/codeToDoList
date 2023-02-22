2ebb456747fd2fa6f21e20eceb734018d31fbb8d
Michel <michelssalves@gmail.com>
2023-02-22 14:18:07 -0300

    2023-02-22

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Menu from '../components/MenuComp.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Menu',
+    component: Menu
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
